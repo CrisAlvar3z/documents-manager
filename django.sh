dffd252446cf02d045bfd97cf3ee310982e0091b
CrisAlvar3z <cris.alvarez.tobal@gmail.com>
2023-09-27 17:26:49 -0300

    Adding test superuser automatically

--- django.sh
+++ django.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+export DJANGO_SUPERUSER_USERNAME="admin"
+export DJANGO_SUPERUSER_PASSWORD="admin"
+export DJANGO_SUPERUSER_EMAIL="admin@admin.com"
+
 echo "Create migrations"
-python manage.py makemigrations gestor_documentos_api
+python manage.py makemigrations api
 echo "=================================="
 
 echo "Migrate"
 python manage.py migrate
+echo "=================================="
+
+echo "Init Admin"
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 echo "=================================="
 
 echo "Start server"
