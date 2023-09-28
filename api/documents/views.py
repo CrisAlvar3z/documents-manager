c42a291b60f5fcf71e5135769d16b9e7aadaccab
CrisAlvar3z <cris.alvarez.tobal@gmail.com>
2023-09-28 00:14:15 -0300

    Adding Patch/Delete/Get methods

--- api/documents/views.py
+++ api/documents/views.py
@@ -1,12 +1,14 @@
+
 from .serializers import DocumentSerializer
 from .models import Document
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import generics
 # Create your views here.
 
-class DocumentView(APIView):
+class DocumentViewGetPost(APIView):
     parser_classes = (MultiPartParser, FormParser)
 
     def get(self, request, *args, **kwargs):
@@ -16,6 +18,34 @@ class DocumentView(APIView):
 
     def post(self, request, *args, **kwargs):
         Documents_serializer = DocumentSerializer(data=request.data)
+        if Documents_serializer.is_valid():
+            Documents_serializer.save()
+            return Response(Documents_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            print('error', Documents_serializer.errors)
+            return Response(Documents_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DocumentViewUpdateDelete(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def delete(self, request, *args, **kwargs):
+        #delete by id
+        Documents = Document.objects.get(pk=kwargs['pk'])
+        #catch if document does not exist
+        #now delete from filesystem
+        Documents.file.delete()
+        Documents.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def patch(self, request, *args, **kwargs):
+        #update by id and remove old file and replace with new one
+        Documents = Document.objects.get(pk=kwargs['pk'])
+        Documents_serializer = DocumentSerializer(Documents, data=request.data, partial=True)
+        #prevent MultiValueDictKeyError exception
+        if 'file' in request.data:
+            Documents.file.delete()
+        
         if Documents_serializer.is_valid():
             Documents_serializer.save()
             return Response(Documents_serializer.data, status=status.HTTP_201_CREATED)
