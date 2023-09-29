0073127398aaada67111b5a9e0757dd3287ae81d
CrisAlvar3z <cris.alvarez.tobal@gmail.com>
2023-09-29 03:10:08 -0300

    Allow blank in Document description value

--- api/documents/models.py
+++ api/documents/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Document(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.CharField(blank=True, max_length=255)
     file = models.FileField(upload_to='documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
